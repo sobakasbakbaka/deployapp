023849f3de18340b6ef2383ce0e68ca4d21db6c9
sobakasbakbaka <sobakasbakbaka@yandex.kz>
2023-11-25 00:54:51 +0300

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,29 @@
-export const App = () => <h1>Hello biliboba!</h1>;
+import { useState } from "react";
+import styles from "./app.module.css";
+
+export const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncClick = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
+  const handleDecClick = () => {
+    setCounter((prevState) => prevState - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <div className={styles.counter}>
+        <button className={styles.button} onClick={handleDecClick}>
+          -
+        </button>
+        <p className={styles.number}>{counter}</p>
+        <button className={styles.button} onClick={handleIncClick}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
